11f8ada1dad82b877488c75c2cda48824d6987ab
Tomas Rutkauskas <tomas@baltsoft.com>
2025-10-15 13:27:35 +0300

    Add ConvertPdfToPdfA example project for PDF to PDF/A conversion
    Introduced a new example project, ConvertPdfToPdfA, demonstrating how to convert PDF files to PDF/A format with embedded invoice XML using the ConvertAPI library. Updated the solution file to include the new project.

---
+++ Examples/ConvertPdfToPdfA/Program.cs
@@ -0,0 +1,43 @@
+﻿using ConvertApiDotNet;
+using ConvertApiDotNet.Exceptions;
+
+namespace ConvertPdfToPdfA
+{
+    class Program
+    {
+        /// <summary>
+        /// Example of converting a PDF file to PDF/A with embedded invoice XML using .NET C#
+        /// https://www.convertapi.com/pdf-to-pdfa/csharp
+        /// </summary>
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                // Get your API Token at https://www.convertapi.com/a/authentication
+                var convertApi = new ConvertApi("api_token");
+
+                const string sourceFile = @"..\..\..\..\TestFiles\invoice.pdf";
+                const string invoiceXml = @"..\..\..\..\TestFiles\invoice.xml";
+
+                var destinationFileName = Path.Combine(Path.GetTempPath(), $"result-pdfa-{Guid.NewGuid()}.pdf");
+
+                // Convert PDF to PDF/A
+                var convertTask = await convertApi.ConvertAsync("pdf", "pdfa",
+                    new ConvertApiFileParam(sourceFile),
+                    new ConvertApiParam("InvoiceFormat", "zugferd1"),
+                    new ConvertApiFileParam("InvoiceFile", invoiceXml));
+
+                var savedFile = await convertTask.Files.First().SaveFileAsync(destinationFileName);
+
+                Console.WriteLine("The converted PDF/A saved to " + savedFile);
+            }
+            // Catch exceptions from asynchronous methods
+            catch (ConvertApiException e)
+            {
+                Console.WriteLine("Status Code: " + e.StatusCode);
+                Console.WriteLine("Response: " + e.Response);
+            }
+            Console.ReadLine();
+        }
+    }
+}
